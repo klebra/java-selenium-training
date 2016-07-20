1c939132da07e8530d78246eb6abda83018344bf
Alexey Krutin <rhenby@gmail.com>
2016-07-20 22:13:30 +0600

    L2_HW3
    Реализованы следующие тесты:
    - AddMovieTest1 - позитивный сценарий для добавления фильма
    - AddMovieTest2 - негативный сценарий для добавления фильма
    - DeleteMovieTest - сценарий для удаления фильма

---
+++ src/test/java/ru/stqa/selenium/DeleteMovieTest.java
@@ -0,0 +1,31 @@
+package ru.stqa.selenium;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import static org.testng.Assert.assertTrue;
+
+public class DeleteMovieTest extends TestNgTestBase {
+
+    @Test
+    public void deleteMovie() {
+        WebDriverWait wait = new WebDriverWait(driver, 2);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("results")));
+        Integer resultsCounter = driver.findElement(By.id("results")).findElements(By.tagName("a")).size();
+        if (resultsCounter > 0){
+            driver.findElements(By.className("title")).get(0).click();
+        } else {
+            Assert.fail("No movies to delete");
+        }
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("img[alt=\"Remove\"]")));
+        driver.findElement(By.cssSelector("img[alt=\"Remove\"]")).click();
+        Alert deleteConfirmation = driver.switchTo().alert();
+        deleteConfirmation.accept();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("results")));
+        Integer resultsCounterAfter = driver.findElement(By.id("results")).findElements(By.tagName("a")).size();
+        assertTrue(resultsCounterAfter.equals(resultsCounter - 1));
+    }
+}
--- src/test/java/ru/stqa/selenium/LoginTest.java
+++ src/test/java/ru/stqa/selenium/LoginTest.java
@@ -8,10 +8,10 @@ public class LoginTest extends TestNgTestBase{
   @Test
   public void testLogin() throws Exception {
     driver.get(baseUrl + "/php4dvd/");
-    driver.findElement(By.name("password")).clear();
-    driver.findElement(By.name("password")).sendKeys("admin");
     driver.findElement(By.id("username")).clear();
     driver.findElement(By.id("username")).sendKeys("admin");
+    driver.findElement(By.name("password")).clear();
+    driver.findElement(By.name("password")).sendKeys("admin");
     driver.findElement(By.name("submit")).click();
   }
 
